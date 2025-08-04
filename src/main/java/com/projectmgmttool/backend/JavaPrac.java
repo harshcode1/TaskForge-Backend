548875759e51628521920e074832b134883a58ac
harshcode1 <harsh9995soni@gmail.com>
2025-08-04 15:30:06 +0530

    Done with Work Laptop

---
+++ src/main/java/com/projectmgmttool/backend/JavaPrac.java
@@ -0,0 +1,15 @@
+package com.projectmgmttool.backend;
+
+import java.util.*;
+
+public class JavaPrac {
+
+    public static void main(String[] args) {
+        Set<Integer> st = new TreeSet<>();
+        st.add(13);
+        st.add(2);
+        st.add(21);
+
+        System.out.println(st);
+    }
+}
